4f22d6a2eb2f11f57eb533e9be957753b58c00d3
Rahul Kumar <Rahul Kumar>
2017-06-12 13:01:42 +0530

    A simple music player for android using sliding panel layout similar to google play music

---
+++ app/src/main/java/net/trellisys/audioplayer/MainActivity.java
@@ -0,0 +1,300 @@
+package net.trellisys.audioplayer;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import net.trellisys.audioplayer.slidinguppanelhelper.PlayPauseView;
+import net.trellisys.audioplayer.slidinguppanelhelper.SlidingUpPanelLayout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static Context mContext;
+
+    public static final String SONG_TITLE = "songTitle";
+    public static final String DISPLAY_NAME = "displayName";
+    public static final String SONG_ID = "songID";
+    public static final String SONG_PATH = "songPath";
+    public static final String ALBUM_NAME = "albumName";
+    public static final String ARTIST_NAME = "artistName";
+    public static final String SONG_DURATION = "songDuration";
+    public static final String SONG_POS = "songPosition";
+
+    private ListView recycler_songslist;
+    private AllSongListAdapter mAllSongsListAdapter;
+    private SlidingUpPanelLayout mLayout;
+    private ImageView songAlbumbg, img_bottom_slideone, img_bottom_slidetwo,
+            imgbtn_backward, imgbtn_forward;
+    private PlayPauseView btn_playpause, btn_playpausePanel;
+    private TextView txt_timeprogress, txt_timetotal, txt_playesongname,
+            txt_songartistname, txt_playesongname_slidetoptwo, txt_songartistname_slidetoptwo;
+
+    private RelativeLayout slidepanelchildtwo_topviewone, slidepanelchildtwo_topviewtwo;
+    private boolean isExpand = false;
+    private SharedPreferences sharedPref;
+    private PlaybackManager playbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        mContext = this;
+        setContentView(R.layout.activity_main);
+        init();
+        initListeners();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            playbackManager = new PlaybackManager(mContext);
+        } else {
+            requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            }, 0);
+        }
+    }
+
+    public static Context getInstance() {
+        return mContext;
+    }
+
+    private void init() {
+        recycler_songslist = (ListView) findViewById(R.id.recycler_allSongs);
+        mLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        songAlbumbg = (ImageView) findViewById(R.id.image_songAlbumbg_mid);
+        img_bottom_slideone = (ImageView) findViewById(R.id.img_bottom_slideone);
+        img_bottom_slidetwo = (ImageView) findViewById(R.id.img_bottom_slidetwo);
+
+        txt_timeprogress = (TextView) findViewById(R.id.slidepanel_time_progress);
+        txt_timetotal = (TextView) findViewById(R.id.slidepanel_time_total);
+        imgbtn_backward = (ImageView) findViewById(R.id.btn_backward);
+        imgbtn_forward = (ImageView) findViewById(R.id.btn_forward);
+
+        btn_playpause = (PlayPauseView) findViewById(R.id.btn_play);
+        btn_playpausePanel = (PlayPauseView) findViewById(R.id.bottombar_play);
+
+        btn_playpausePanel.Pause();
+        btn_playpause.Pause();
+        btn_playpausePanel.setOnClickListener(this);
+        btn_playpause.setOnClickListener(this);
+
+        TypedValue typedvaluecoloraccent = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorAccent, typedvaluecoloraccent, true);
+        imgbtn_backward.setOnClickListener(this);
+        imgbtn_forward.setOnClickListener(this);
+
+
+        txt_playesongname = (TextView) findViewById(R.id.txt_playesongname);
+        txt_songartistname = (TextView) findViewById(R.id.txt_songartistname);
+        txt_playesongname_slidetoptwo = (TextView) findViewById(R.id.txt_playesongname_slidetoptwo);
+        txt_songartistname_slidetoptwo = (TextView) findViewById(R.id.txt_songartistname_slidetoptwo);
+
+        slidepanelchildtwo_topviewone = (RelativeLayout) findViewById(R.id.slidepanelchildtwo_topviewone);
+        slidepanelchildtwo_topviewtwo = (RelativeLayout) findViewById(R.id.slidepanelchildtwo_topviewtwo);
+
+        slidepanelchildtwo_topviewone.setVisibility(View.VISIBLE);
+        slidepanelchildtwo_topviewtwo.setVisibility(View.INVISIBLE);
+
+        slidepanelchildtwo_topviewone.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+
+            }
+        });
+
+        slidepanelchildtwo_topviewtwo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+
+            }
+        });
+
+    }
+
+    private void initListeners() {
+
+        mLayout.setPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                if (slideOffset == 0.0f) {
+                    isExpand = false;
+                    slidepanelchildtwo_topviewone.setVisibility(View.VISIBLE);
+                    slidepanelchildtwo_topviewtwo.setVisibility(View.INVISIBLE);
+                } else if (slideOffset > 0.0f && slideOffset < 1.0f) {
+                    // if (isExpand) {
+                    // slidepanelchildtwo_topviewone.setAlpha(1.0f);
+                    // slidepanelchildtwo_topviewtwo.setAlpha(1.0f -
+                    // slideOffset);
+                    // } else {
+                    // slidepanelchildtwo_topviewone.setAlpha(1.0f -
+                    // slideOffset);
+                    // slidepanelchildtwo_topviewtwo.setAlpha(1.0f);
+                    // }
+
+                } else {
+                    isExpand = true;
+                    slidepanelchildtwo_topviewone.setVisibility(View.INVISIBLE);
+                    slidepanelchildtwo_topviewtwo.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onPanelExpanded(View panel) {
+                isExpand = true;
+            }
+
+            @Override
+            public void onPanelCollapsed(View panel) {
+                isExpand = false;
+            }
+
+            @Override
+            public void onPanelAnchored(View panel) {
+            }
+
+            @Override
+            public void onPanelHidden(View panel) {
+
+            }
+        });
+
+        recycler_songslist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                HashMap<String, String> hashMap = PlaybackManager.songsList.get(position);
+                loadSongInfo(hashMap);
+                PlaybackManager.playSong(hashMap.get(SONG_PATH));
+                btn_playpause.Play();
+                btn_playpausePanel.Play();
+            }
+        });
+    }
+
+    public void setAllSongs() {
+        mAllSongsListAdapter = new AllSongListAdapter(mContext, PlaybackManager.songsList);
+        recycler_songslist.setAdapter(mAllSongsListAdapter);
+        PlaybackManager.songsList = mAllSongsListAdapter.getNewList();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.bottombar_play:
+                playPauseEvent();
+                break;
+
+            case R.id.btn_play:
+                playPauseEvent();
+                break;
+
+            case R.id.btn_forward:
+                    playbackManager.playNext(false);
+                break;
+
+            case R.id.btn_backward:
+                playbackManager.playPrev();
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    private void playPauseEvent() {
+        if (SongService.isPlaying()) {
+            btn_playpause.Pause();
+            btn_playpausePanel.Pause();
+            startService(new Intent(mContext, SongService.class).setAction(SongService.ACTION_PAUSE));
+        } else {
+            if (!PlaybackManager.getLastPlayingSongPref().get(SONG_ID).equals("")) {
+                startService(new Intent(mContext, SongService.class).setAction(SongService.ACTION_RESUME));
+                btn_playpause.Play();
+                btn_playpausePanel.Play();
+            }
+        }
+    }
+
+    public void loadSongInfo(HashMap<String, String> songDetail) {
+        String title = songDetail.get("songTitle");
+        String artist = songDetail.get("artistName");
+        int milliSecDuration = Integer.parseInt(songDetail.get("songDuration"));
+        if (txt_playesongname != null) {
+            txt_playesongname.setText(title);
+            txt_playesongname_slidetoptwo.setText(title);
+            txt_songartistname.setText(artist);
+            txt_songartistname_slidetoptwo.setText(artist);
+            txt_timetotal.setText(calculateDuration(milliSecDuration));
+        }
+
+        playbackManager.setLastPlayingSongPref(songDetail);
+//        updateProgress(songsManager);
+    }
+
+
+    public String calculateDuration(int millisec) {
+        long sec = millisec / 1000;
+        return sec != 0 ? String.format("%d:%02d", sec / 60, sec % 60) : "-:--";
+    }
+
+//     private void updateProgress(HashMap songDetail) {
+////        if (audio_progress != null) {
+////            // When SeekBar Draging Don't Show Progress
+////            if (!isDragingStart) {
+////                // Progress Value comming in point it range 0 to 1
+////                audio_progress.setValue((int) (mSongDetail.audioProgress * 100));
+////            }
+////            String timeString = String.format("%d:%02d", mSongDetail.audioProgressSec / 60, mSongDetail.audioProgressSec % 60);
+////            txt_timeprogress.setText(timeString);
+////        }
+//    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            playbackManager = new PlaybackManager(mContext);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setPlayPauseView();
+        loadSongInfo(PlaybackManager.getLastPlayingSongPref());
+    }
+
+    private void setPlayPauseView() {
+        if (SongService.isPlaying()) {
+            btn_playpause.Play();
+            btn_playpausePanel.Play();
+        } else {
+            btn_playpause.Pause();
+            btn_playpausePanel.Pause();
+        }
+    }
+}
---
+++ app/src/main/java/net/trellisys/audioplayer/PlaybackManager.java
@@ -0,0 +1,203 @@
+package net.trellisys.audioplayer;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static net.trellisys.audioplayer.MainActivity.ALBUM_NAME;
+import static net.trellisys.audioplayer.MainActivity.ARTIST_NAME;
+import static net.trellisys.audioplayer.MainActivity.SONG_DURATION;
+import static net.trellisys.audioplayer.MainActivity.SONG_ID;
+import static net.trellisys.audioplayer.MainActivity.SONG_PATH;
+import static net.trellisys.audioplayer.MainActivity.SONG_POS;
+import static net.trellisys.audioplayer.MainActivity.SONG_TITLE;
+
+/**
+ * Created by rahul on 5/31/2017.
+ */
+
+public class PlaybackManager {
+
+    private Uri allsongsuri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+    private String[] projectionSongs = {MediaStore.Audio.Media._ID, MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.DATA, MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.DISPLAY_NAME, MediaStore.Audio.Media.DURATION};
+    private String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+    public static ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+    public static final String songPref = "songPref";
+    private static Context mContext;
+    private static SharedPreferences sharedPref;
+
+    public PlaybackManager(Context mContext) {
+        this.mContext = mContext;
+        sharedPref = mContext.getSharedPreferences(songPref, mContext.MODE_PRIVATE);
+        createPlayList();
+    }
+
+    public PlaybackManager(Context mContext, LoadSongListener loadSongListener, Uri allsongsuri) {
+        this.mContext = mContext;
+        sharedPref = mContext.getSharedPreferences(songPref, mContext.MODE_PRIVATE);
+        if(loadSongListener!=null)
+            this.loadSongListener = loadSongListener;
+        if(allsongsuri!=null)
+            this.allsongsuri = allsongsuri;
+        createPlayList();
+    }
+
+//
+//    private FilenameFilter filenameFilter = new FilenameFilter() {
+//        @Override
+//        public boolean accept(File dir, String name) {
+//            return (name.endsWith(".mp3") || name.endsWith(".MP3"));
+//        }
+//    };
+
+    private LoadSongListener loadSongListener = new LoadSongListener() {
+
+        @Override
+        public void onDoneLoading() {
+//            if(mContext.getClass().getSimpleName().equals("MainActivity"))
+                ((MainActivity)mContext).setAllSongs();
+        }
+    };
+
+    private void createPlayList() {
+        new LoadSongsAsync().execute();
+    }
+
+    private class LoadSongsAsync extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Cursor cursor = mContext.getContentResolver().query(allsongsuri, projectionSongs,
+                    selection, null, null);
+
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    do {
+                        HashMap<String, String> song = new HashMap<String, String>();
+                        String song_title = cursor
+                                .getString(cursor
+                                        .getColumnIndex(MediaStore.Audio.Media.TITLE));
+                        String display_name = cursor
+                                .getString(cursor
+                                        .getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+                        int song_id = cursor.getInt(cursor
+                                .getColumnIndex(MediaStore.Audio.Media._ID));
+
+                        String song_path = cursor.getString(cursor
+                                .getColumnIndex(MediaStore.Audio.Media.DATA));
+
+                        String album_name = cursor.getString(cursor
+                                .getColumnIndex(MediaStore.Audio.Media.ALBUM));
+
+                        String artist_name = cursor.getString(cursor
+                                .getColumnIndex(MediaStore.Audio.Media.ARTIST));
+
+                        int song_duration = cursor.getInt(cursor
+                                .getColumnIndex(MediaStore.Audio.Media.DURATION));
+
+                        song.put("songTitle", song_title);
+                        song.put("displayName", display_name);
+                        song.put("songID", "" + song_id);
+                        song.put("songPath", song_path);
+                        song.put("albumName", album_name);
+                        song.put("artistName", artist_name);
+                        song.put("songDuration", "" + song_duration);
+
+                        songsList.add(song);
+
+                    } while (cursor.moveToNext());
+
+                }
+                cursor.close();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            loadSongListener.onDoneLoading();
+        }
+    }
+
+    public static void playSong(String path){
+        Intent i = new Intent(mContext, SongService.class);
+        i.setAction(SongService.ACTION_PLAY);
+        i.putExtra("path", path);
+        mContext.startService(i);
+    }
+
+    public static void playNext(boolean isShuffle){
+
+        int pos = Integer.parseInt(getLastPlayingSongPref().get(SONG_POS));
+        pos += 1;
+        if(pos>-1 && pos<songsList.size()){
+            HashMap<String, String> hashMap = songsList.get(pos);
+            ((MainActivity)mContext).loadSongInfo(hashMap);
+            playSong(hashMap.get(SONG_PATH));
+        }
+    }
+
+    public static void playPrev(){
+        int pos = Integer.parseInt(getLastPlayingSongPref().get(SONG_POS));
+        pos -= 1;
+        if(pos>-1 && pos<songsList.size()){
+            HashMap<String, String> hashMap = songsList.get(pos);
+            ((MainActivity)mContext).loadSongInfo(hashMap);
+            playSong(hashMap.get(SONG_PATH));
+        }
+    }
+
+    public void setLastPlayingSongPref(HashMap<String, String> songDetail) {
+        SharedPreferences.Editor prefEditor = sharedPref.edit();
+        prefEditor.putString(SONG_TITLE, songDetail.get(SONG_TITLE))
+                .putString(SONG_ID, songDetail.get(SONG_ID))
+                .putString(ARTIST_NAME, songDetail.get(ARTIST_NAME))
+                .putString(ALBUM_NAME, songDetail.get(ALBUM_NAME))
+                .putString(SONG_DURATION, songDetail.get(SONG_DURATION))
+                .putString(SONG_PATH, songDetail.get(SONG_PATH))
+                .putString(SONG_POS, songDetail.get(SONG_POS));
+
+        prefEditor.commit();
+    }
+
+    public static HashMap<String, String>  getLastPlayingSongPref() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put(SONG_TITLE, sharedPref.getString(SONG_TITLE, ""));
+        hashMap.put(SONG_ID, sharedPref.getString(SONG_ID, ""));
+        hashMap.put(ARTIST_NAME, sharedPref.getString(ARTIST_NAME, ""));
+        hashMap.put(ALBUM_NAME, sharedPref.getString(ALBUM_NAME, ""));
+        hashMap.put(SONG_DURATION, sharedPref.getString(SONG_DURATION, ""+0));
+        hashMap.put(SONG_PATH, sharedPref.getString(SONG_PATH, ""));
+        hashMap.put(SONG_POS, sharedPref.getString(SONG_POS, ""+(-1)));
+        return hashMap;
+    }
+
+    private static int shufflePos(){
+        return 0;
+    }
+
+    public interface LoadSongListener {
+        public void onDoneLoading();
+    }
+}
